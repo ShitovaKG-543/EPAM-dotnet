4baffc3cace100ab55b5fcaa57c38b6bf32f8023
ShitovaKG-543 <kshitova@inbox.ru>
2018-10-05 23:46:43 +0300

    Solution 03: Changed task 1 and added task 3

---
+++ Shitova_Ksenia_Task03/Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = WorkWithArray.WorkWithArray.GetArrayWithRandomValue(20, -100, 100);
+            StringBuilder resultString = new StringBuilder();
+            resultString.Append($"Initial array: \r\n{WorkWithArray.WorkWithArray.ToString(array)}\r\n");
+            resultString.Append($"Sum of the non-negative elements of the array: {GetArrayNonNegativeElementsSum(array)}");
+            Console.WriteLine(resultString);
+            Console.ReadLine();
+        }
+
+        public static int GetArrayNonNegativeElementsSum(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] >= 0)
+                {
+                    sum += array[i];
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Shitova_Ksenia_Task03/WorkWithArray/WorkWithArray.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+
+namespace WorkWithArray
+{
+    public static class WorkWithArray
+    {
+        public static int[] GetArrayWithRandomValue(int numberElements, int minValue, int maxValue)
+        {
+            Random rand = new Random();
+            int[] array = new int[numberElements];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(minValue, maxValue);
+            }
+            return array;
+        }
+
+        public static int GetMaxValue(int[] array)
+        {
+            int max = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (max < array[i])
+                {
+                    max = array[i];
+                }
+            }
+            return max;
+        }
+
+        public static int GetMinValue(int[] array)
+        {
+            int min = 0;
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (min > array[i])
+                {
+                    min = array[i];
+                }
+            }
+            return min;
+        }
+
+        public static void SortArray(ref int[] array)
+        {
+            int temp;
+            int[] sortedArray = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 1 + i; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+        }
+
+        public static string ToString(int[] array)
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (int elArray in array)
+            {
+                result.Append($"{elArray} ");
+            }
+            return result.ToString();
+        }
+    }
+}
