bad43eb4e27633e2d71ebc9215db7df164c57ea0
ShitovaKG-543 <kshitova@inbox.ru>
2018-10-08 10:27:56 +0300

    Added solution 04: task 1 and task 2

---
+++ Shitova_Ksenia_Task04/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string:");
+            string initialString = Console.ReadLine();
+            string pattern = @"\W+";
+            string[] elements = Regex.Split(initialString, pattern);
+            int sum = 0;
+            foreach (string element in elements)
+            {
+                sum += element.Length;
+            }
+            Console.WriteLine($"Average length of word: { sum / elements.Length }");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Shitova_Ksenia_Task04/Task2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first string: ");
+            string initialString1 = Console.ReadLine();
+            Console.Write("Enter the secong string: ");
+            string initialString2 = Console.ReadLine();
+
+            string initialString2WithoutRepeat = GetStringWithoutRepeat(initialString2);
+            DoubleString1CharactersBelongingToString2(ref initialString1, initialString2WithoutRepeat);
+
+            Console.WriteLine($"Resulting string: {initialString1}");
+            Console.ReadLine();
+        }
+
+        public static string GetStringWithoutRepeat(string initialString)
+        {
+            string initialStringWithoutRepeat = String.Empty;
+            for (int i = 0; i < initialString.Length; i++)
+            {
+                if (!initialStringWithoutRepeat.Contains(initialString[i]))
+                {
+                    initialStringWithoutRepeat += initialString[i];
+                }
+            }
+            return initialStringWithoutRepeat;
+        }
+
+        public static void DoubleString1CharactersBelongingToString2(ref string string1, string string2)
+        {
+            for (int i = 0; i < string2.Length; i++)
+            {
+                for (int j = 0; j < string1.Length; j++)
+                {
+                    if (string1[j].Equals(string2[i]))
+                    {
+                        string1 = string1.Insert(j + 1, string2[i].ToString());
+                        j++;
+                    }
+                }
+            }
+        }
+
+    }
+}
