f8911d97a175960581032d03451d4f46279b7824
ShitovaKG-543 <kshitova@inbox.ru>
2018-11-06 15:53:23 +0300

    Added solution 12 task 1

---
+++ Shitova_Ksenia_Task12/Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "disposable_task_file.txt";
+
+            try
+            {
+                string[] dataFile = File.ReadAllLines(filename);
+
+                for (int i = 0; i < dataFile.Length; i++)
+                {
+                    int number;
+                    if (int.TryParse(dataFile[i], out number))
+                    {
+                        dataFile[i] = Math.Pow(number, 2).ToString();
+                    }
+                }
+
+                File.WriteAllLines(filename, dataFile);
+
+                Console.WriteLine("The program completed successfully.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred during the operation of the program:");
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
